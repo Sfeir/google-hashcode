16832220aa58b98c8eb2bed3a190647fcf6acb1b
bcornu <cornu.b@sfeir.com>
2017-02-23 18:59:19 +0100

    model

--- src/main/java/com/sfeir/hashcode/model/Endpoint.java
+++ src/main/java/com/sfeir/hashcode/model/Endpoint.java
@@ -1,5 +1,8 @@
 package com.sfeir.hashcode.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by bcornu on 2/23/17.
  */
@@ -7,6 +10,22 @@ public class Endpoint {
 
     private int id;
     private int datacenterLatency;
+    private Map<Cache, Integer> caches = new HashMap<>();
 
+    public Endpoint(int id, int datacenterLatency) {
+        this.id = id;
+        this.datacenterLatency = datacenterLatency;
+    }
 
+    public int getDatacenterLatency() {
+        return datacenterLatency;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Map<Cache, Integer> getCaches() {
+        return caches;
+    }
 }
