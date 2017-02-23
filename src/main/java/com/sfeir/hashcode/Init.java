bdc2b2b613e32c5c557d8a9b3be4f4ce1ab8bd58
Tony Proum <t.proum@gmail.com>
2017-02-23 19:04:47 +0100

    Init

---
+++ src/main/java/com/sfeir/hashcode/Init.java
@@ -0,0 +1,35 @@
+package com.sfeir.hashcode;
+
+/**
+ * Created by tonyproum on 23/02/17.
+ */
+public class Init {
+
+
+    private final String initLine;
+
+    public Init(String initLine){
+        this.initLine = initLine;
+    }
+
+    public int numberOfVideos(){
+        return Integer.valueOf(initLine.split(" ")[0]);
+    }
+
+    public int numberOfEndpoints(){
+        return Integer.valueOf(initLine.split(" ")[1]);
+    }
+
+    public int numberOfRequestDescription(){
+        return Integer.valueOf(initLine.split(" ")[2]);
+    }
+
+    public int numberOfCaches(){
+        return Integer.valueOf(initLine.split(" ")[3]);
+    }
+
+    public int cacheSize(){
+        return Integer.valueOf(initLine.split(" ")[4]);
+    }
+
+}
