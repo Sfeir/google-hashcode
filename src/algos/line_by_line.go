8cba340170a82afa4017e74cb6bbf36747582720
Vincent Dolez <dolez.v@sfeir.com>
2018-02-26 21:01:31 +0100

    feat: line by line

---
+++ src/algos/line_by_line.go
@@ -0,0 +1,15 @@
+package algos
+
+import (
+	"github.com/Sfeir/google-hashcode-lille/src/model"
+	logger "github.com/sirupsen/logrus"
+)
+
+func LineByLine(inputs *model.Inputs) []model.Slice {
+	for _, row := range inputs.Cells {
+		for _, cell := range row {
+			logger.Debug(cell)
+		}
+	}
+	return []model.Slice{}
+}
