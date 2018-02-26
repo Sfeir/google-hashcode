c85a01f09a52b938ffcc01b7c1f8e8377bdc7c6c
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-02-26 20:57:52 +0100

    Create empty skeleton of min shapes.

--- src/algos/min_shapes_algo.go
+++ src/algos/min_shapes_algo.go
@@ -2,6 +2,12 @@ package algos
 
 import "github.com/Sfeir/google-hashcode-lille/src/model"
 
+func calculateAvailableShapes(size int) [][]int {
+	// TODO(sylvain) return [](row,columns)
+	return [][]int{}
+}
+
 func MinShapesAlgo(inputs *model.Inputs) []model.Slice {
-	
+
+	return []model.Slice{}
 }
