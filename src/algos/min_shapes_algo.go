0a5d5abaa34b62f82628d5e9b4456062adeb8ab7
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2018-02-26 21:18:45 +0100

    Implement a basic minshape.

--- src/algos/min_shapes_algo.go
+++ src/algos/min_shapes_algo.go
@@ -7,18 +7,35 @@ type shape struct {
 }
 
 func calculateAvailableShapes(size int) []shape {
-	// TODO(sylvain) return [](row,columns)
-	return []shape{}
+	var result = []shape{}
+	result = append(result, shape{row: 1, column: size})
+	result = append(result, shape{row: size, column: 1})
+	// TODO(sylvain) finish the rectangles and square shapes.
+	return result
 }
 
 func shapeCanFit(s shape, c [][]model.Cell, x int, y int) bool {
-	// TODO(sylvain) return true if all the cells between (x,y) and that fits in S are available
-	return false
+	if (x+s.column > len(c[0])) || (y+s.row > len(c)) {
+		return false
+	}
+
+	for i := x; i < x+s.row; i++ {
+		for j := y; j < y+s.column; j++ {
+			if !c[i][j].Taken {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 func declareAllCellsTaken(s shape, c [][]model.Cell, x int, y int) model.Slice {
-	// TODO(sylvain) set all the taken to true for cell that are in s starting from (x,y)
-	return model.Slice{}
+	for i := x; i < x+s.row; i++ {
+		for j := y; j < y+s.column; j++ {
+			c[i][j].Taken = true
+		}
+	}
+	return model.Slice{StartX: x, StartY: y, EndX: x + s.row, EndY: y + s.column}
 }
 
 func MinShapesAlgo(inputs *model.Inputs) []model.Slice {
@@ -27,7 +44,7 @@ func MinShapesAlgo(inputs *model.Inputs) []model.Slice {
 	for x := 0; x < inputs.NbRows; x++ {
 		for y := 0; y < inputs.NbColumns; y++ {
 			for _, s := range availableShapes {
-				if (shapeCanFit(s, inputs.Cells, x, y)) {
+				if shapeCanFit(s, inputs.Cells, x, y) {
 					result = append(result, declareAllCellsTaken(s, inputs.Cells, x, y))
 				}
 			}
