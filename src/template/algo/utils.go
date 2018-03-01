a5aafca595278ca8c8afc02de1daa0633dacc220
Vincent Dolez <dolez.v@sfeir.com>
2018-03-01 20:07:05 +0100

    utils

---
+++ src/template/algo/utils.go
@@ -0,0 +1,23 @@
+package algo
+
+import (
+	"math"
+
+	"github.com/Sfeir/google-hashcode-lille/src/model"
+)
+
+func Distance(x1, y1, x2, y2 int) int {
+	return int(math.Abs(float64(x2-x1)) + math.Abs(float64(y2-y1)))
+}
+
+func TrajetPossible(heureCourante int, taxi model.Taxi, ride model.Ride) bool {
+	distTaxiRide := Distance(taxi.ColumnPos, taxi.RowPos, ride.BeginColumn, ride.BeginRow)
+	distRide := Distance(ride.BeginColumn, ride.BeginRow, ride.EndColumn, ride.EndRow)
+	distHorraire := ride.LatestFinish - (heureCourante + distRide + distTaxiRide)
+	return distHorraire >= 0
+}
+
+func TempsMaxAvantRetard(heureCourante int, ride model.Ride) int {
+	//distRide := Distance(ride.BeginColumn, ride.BeginRow, ride.EndColumn, ride.EndRow)
+	return 0
+}
