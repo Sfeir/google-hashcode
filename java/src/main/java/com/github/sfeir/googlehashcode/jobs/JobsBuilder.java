3dbb02efabe01cb8dae9e0120dc491c36b34e990
Rémy Gardette <github@remyg.fr>
2019-02-28 20:33:31 +0100

    jobs builder

---
+++ java/src/main/java/com/github/sfeir/googlehashcode/jobs/JobsBuilder.java
@@ -0,0 +1,25 @@
+package com.github.sfeir.googlehashcode.jobs;
+
+import com.github.sfeir.googlehashcode.compute.Job;
+import com.github.sfeir.googlehashcode.output.Slide;
+
+import java.util.*;
+
+public class JobsBuilder {
+
+    public Queue<Job> build(List<Slide> slides) {
+
+        Queue<Job> queue = new ArrayDeque<>();
+
+        queue.add(new Job(new ArrayList<>(slides)));
+
+        for (int i = 1; i < slides.size(); i++) {
+            List<Slide> list = slides.subList(i, slides.size());
+            list.addAll(slides.subList(0, i));
+            queue.add(new Job(list));
+        }
+
+        return queue;
+    }
+
+}
