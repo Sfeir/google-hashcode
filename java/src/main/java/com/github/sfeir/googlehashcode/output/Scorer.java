d667dcb22a81bb0ebdc0a6f7c9335874cf7e3c71
Rémy Gardette <remyg@localhost.localdomain>
2019-02-28 19:15:35 +0100

    slides & scoring

---
+++ java/src/main/java/com/github/sfeir/googlehashcode/output/Scorer.java
@@ -0,0 +1,23 @@
+package com.github.sfeir.googlehashcode.output;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Scorer {
+
+    public static int score(Slide slide1, Slide slide2) {
+        Set<String> tags1 = slide1.getTags();
+        Set<String> tags2 = slide2.getTags();
+
+        Set<String> intersection = new HashSet<>(tags1); // use the copy constructor
+        intersection.retainAll(tags2);
+
+        return Math.min(
+                tags1.size() - intersection.size(),
+                Math.min(
+                    tags2.size() - intersection.size(),
+                    intersection.size()
+                )
+        );
+    }
+}
---
+++ java/src/main/java/com/github/sfeir/googlehashcode/output/Slide.java
@@ -0,0 +1,17 @@
+package com.github.sfeir.googlehashcode.output;
+
+import com.github.sfeir.googlehashcode.input.Photo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Slide {
+
+    private Photo photo1;
+    private Photo photo2;
+
+    public Set<String> getTags() {
+        return new HashSet<>();
+    }
+
+}
